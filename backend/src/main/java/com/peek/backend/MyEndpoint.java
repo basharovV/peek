497f248cde68b43733d4fdd5f44557d36f4ace5c
Vyacheslav Basharov <slavbacharov@gmail.com>
2015-10-14 20:59:19 +0100

    Simple Google Cloud Endpoint backend

---
+++ backend/src/main/java/com/peek/backend/MyEndpoint.java
@@ -0,0 +1,40 @@
+/*
+   For step-by-step instructions on connecting your Android application to this backend module,
+   see "App Engine Java Endpoints Module" template documentation at
+   https://github.com/GoogleCloudPlatform/gradle-appengine-templates/tree/master/HelloEndpoints
+*/
+
+package com.peek.backend;
+
+import com.google.api.server.spi.config.Api;
+import com.google.api.server.spi.config.ApiMethod;
+import com.google.api.server.spi.config.ApiNamespace;
+
+import javax.inject.Named;
+
+/**
+ * An endpoint class we are exposing
+ */
+@Api(
+        name = "myApi",
+        version = "v1",
+        namespace = @ApiNamespace(
+                ownerDomain = "backend.peek.com",
+                ownerName = "backend.peek.com",
+                packagePath = ""
+        )
+)
+public class MyEndpoint {
+
+    /**
+     * A simple endpoint method that takes a name and says Hi back
+     */
+    @ApiMethod(name = "sayHi")
+    public Place sayHi(@Named("name") String name) {
+        Place response = new Place();
+        response.setName("Hi, " + name);
+        System.out.print(response.getName());
+        return response;
+    }
+
+}
